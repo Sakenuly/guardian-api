9f41a91f326db9211a1bbe88f7038ac18f9499a5
Sakenuly <sakenuly2016@yandex.ru>
2023-09-24 16:17:35 +0600

    created spinner component

---
+++ app/components/spinner/spinner.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Puff } from 'react-loader-spinner';
+
+function SpinnerLoading() {
+	return (
+		<div>
+			<Puff
+				height='80'
+				width='80'
+				radius={1}
+				color='#4fa94d'
+				ariaLabel='puff-loading'
+				wrapperStyle={{}}
+				wrapperClass=''
+				visible
+			/>
+		</div>
+	);
+}
+
+export { SpinnerLoading };
