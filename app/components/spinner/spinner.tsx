b976fcf7448ece1c62bdd2f8ea6637a58cacaf3d
Sakenuly <sakenuly2016@yandex.ru>
2023-09-26 15:28:59 +0600

    spinner styled with position absolute

--- app/components/spinner/spinner.tsx
+++ app/components/spinner/spinner.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Puff } from 'react-loader-spinner';
-import './style.scss'
+import './style.scss';
 
 function SpinnerLoading() {
 	return (
 		<div className='spinner-container'>
-			<Puff
-				height={80}
-				width={80}
-				radius={1}
-				color='#4fa94d'
-				ariaLabel='puff-loading'
-				wrapperStyle={{}}
-				wrapperClass=''
-				visible
-			/>
+			<div className='spinner'>
+				<Puff
+					height={80}
+					width={80}
+					radius={1}
+					color='#4fa94d'
+					ariaLabel='puff-loading'
+					wrapperStyle={{}}
+					wrapperClass=''
+					visible
+				/>
+			</div>
 		</div>
 	);
 }
