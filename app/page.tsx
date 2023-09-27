0728592436a2c5442145d76906694154c349d0e0
Sakenuly <sakenuly2016@yandex.ru>
2023-09-27 14:55:48 +0600

    If the path changes the spinner turns off

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,25 @@
 
 import { NewsGrid } from '@/app/components/news_grid';
 import { SortMain } from '@/app/components/sort_main';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from './GlobalRedux/hooks/hooks';
+import { SpinnerLoading } from './components/spinner/spinner';
+import { spinnerIsActive } from './GlobalRedux/features/spinner/spinner';
 
 export default function Home() {
+	const pathName = usePathname();
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		dispatch(spinnerIsActive(false));
+	}, [pathName, dispatch]);
+
+	const selector = useAppSelector((state) => state.spinnerSLice);
 	return (
 		<div className='home'>
 			<div className='home__container'>
+				{selector && <SpinnerLoading />}
 				<SortMain />
 				<NewsGrid />
 			</div>
